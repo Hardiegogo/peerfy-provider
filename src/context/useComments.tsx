4661cb2876d257c148f5da2bf405ca668d20dcb3
Shivank12batra <Shivank56batra@gmail.com>
2024-01-10 23:45:59 +0530

    feat: comment display ui change

--- src/context/useComments.tsx
+++ src/context/useComments.tsx
@@ -9,6 +9,7 @@ import { fetchComments } from "../services/comment-services/fetchComments";
 import CommentCreator from "../components/CommentCreator/CommentCreator";
 import { IComment } from "./types";
 import useSocketForComments from "../hooks/useSocketForComments";
+import ToggleButton from "../components/ToggleButton/ToggleButton";
 
 interface MyContextProps {
   comments: IComment[] | undefined;
@@ -25,6 +26,32 @@ export const CommentsProvider = ({ children }: { children: ReactNode }) => {
   const [comments, setComments] = useState<IComment[] | undefined>();
   const [latestActivityFromSocket, setLatestActivityFromSocket] = useState();
   const [socket] = useSocketForComments(setLatestActivityFromSocket);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const [hoveredCommentId, setHoveredCommentId] = useState<string | null>(null);
+
+  const handleMouseEnter = (
+    commentId: string,
+    event: React.MouseEvent<HTMLDivElement>
+  ) => {
+    setHoveredCommentId(commentId);
+    if (event.currentTarget instanceof HTMLDivElement) {
+      event.currentTarget.style.width = "140px"; // Adjust the expanded width
+      event.currentTarget.style.height = "100px"; // Adjust the expanded width
+      event.currentTarget.style.borderRadius = "10%"; // Remove roundedness
+      event.currentTarget.style.overflow = "visible"; // Show hidden content
+    }
+  };
+
+  const handleMouseLeave = (event: React.MouseEvent<HTMLDivElement>) => {
+    setHoveredCommentId(null);
+    if (event.currentTarget instanceof HTMLDivElement) {
+      event.currentTarget.style.width = "40px";
+      event.currentTarget.style.height = "40px";
+      event.currentTarget.style.borderRadius = "50%";
+      event.currentTarget.style.overflow = "hidden";
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -52,12 +79,40 @@ export const CommentsProvider = ({ children }: { children: ReactNode }) => {
                 position: "absolute",
                 left: comment.locationX,
                 top: comment.locationY,
-                backgroundColor: "#3498db",
-                padding: "8px",
-                color: "#ffffff", // Text color
+                width: "40px", // Adjust the initial width as needed
+                height: "40px", // Adjust the initial height as needed
+                borderRadius: "50%", // Make it a circle
+                backgroundColor: "black",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                cursor: "pointer",
+                overflow: "hidden", // Hide overflow content
+                transition: "width 0.3s, height 0.3s, border-radius 0.3s", // Smooth transition effect
               }}
+              onMouseEnter={(e) => handleMouseEnter(comment._id, e)}
+              onMouseLeave={(e) => handleMouseLeave(e)}
             >
-              <p>{comment.content}</p>
+              <div
+                style={{
+                  color: "#ffffff",
+                  margin: 0,
+                  padding: 2,
+                  display: hoveredCommentId === comment._id ? "block" : "none",
+                }}
+              >
+                <p>Shivank</p>
+                <p style={{ fontSize: "14px" }}>{comment.content}</p>
+              </div>
+              <p
+                style={{
+                  color: "#ffffff",
+                  margin: 0,
+                  display: hoveredCommentId !== comment._id ? "block" : "none",
+                }}
+              >
+                S
+              </p>
             </div>
           ))}
         </ul>
@@ -68,8 +123,13 @@ export const CommentsProvider = ({ children }: { children: ReactNode }) => {
   return (
     <CommentsContext.Provider value={{ comments, setComments }}>
       {children}
-      <RenderComments />
+      <ToggleButton isChecked={isChecked} setIsChecked={setIsChecked} />
       <CommentCreator />
+      {isChecked && (
+        <>
+          <RenderComments />
+        </>
+      )}
     </CommentsContext.Provider>
   );
 };
