679240f984cc2529818366222da53a4a803c5b93
Shivank Batra <84995784+Shivank12batra@users.noreply.github.com>
2024-01-10 23:46:43 +0530

    feat: comment display ui change (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import CommentCreator from "./components/CommentCreator/CommentCreator";
-import ToggleButton from "./components/ToggleButton/ToggleButton";
-import { CommentsProvider } from "./context/useComments";
-
-const CommentsProviderWrapper = ({
-  children,
-  isEnabled,
-}: {
-  children: any;
-  isEnabled: boolean;
-}) => {
-  // Wrap the provider in a conditional
-  return isEnabled ? <CommentsProvider>{children}</CommentsProvider> : children;
-};
 
 function App() {
-  // const [isChecked, setIsChecked] = useState(false);
-
-  return (
-    <>
-      Hello Dostonnn!
-    </>
-  );
+  return <>Hello Dostonnn!</>;
 }
 
 export default App;
