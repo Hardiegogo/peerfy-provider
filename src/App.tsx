17767308ecba8afd19eb43f3061833718ce3d563
Shivank12batra <Shivank56batra@gmail.com>
2024-01-10 09:23:43 +0530

    feat: comments provider toggle switch

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import CommentCreator from "./components/CommentCreator/CommentCreator";
 import ToggleButton from "./components/ToggleButton/ToggleButton";
+import { CommentsProvider } from "./context/useComments";
+
+const CommentsProviderWrapper = ({
+  children,
+  isEnabled,
+}: {
+  children: any;
+  isEnabled: boolean;
+}) => {
+  // Wrap the provider in a conditional
+  return isEnabled ? <CommentsProvider>{children}</CommentsProvider> : children;
+};
 
 function App() {
   const [isChecked, setIsChecked] = useState(false);
+
   return (
     <>
       Hello Dostonnn!
       <ToggleButton isChecked={isChecked} setIsChecked={setIsChecked} />
-      {isChecked && <CommentCreator />}
+      {/* Wrap App contents with CommentsProviderWrapper */}
+      <CommentsProviderWrapper isEnabled={isChecked}>
+        {isChecked && <CommentCreator />}
+      </CommentsProviderWrapper>
     </>
   );
 }
